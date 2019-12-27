71f5a1aec102db279ff2b1849cbfcb394c42b765
Gustavo Porto <portothree@gmail.com>
2019-12-27 19:40:08 +0000

    :recycle: using event emitter

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,55 @@
-(() => {
-	const input = document.querySelector(".card-input");
-	const card = document.querySelectorAll(".card");
+const input = document.querySelector(".card-input");
+const card = document.querySelectorAll(".card");
+const obj = new CreditCard();
 
-	if (card) {
-		const obj = new CreditCard();
-		input.addEventListener("input", () => {
-			let vendor = obj.getCreditCardNameByNumber(input.value);
+class EventEmitter {
+	constructor() {
+		this.events = {};
+	}
 
-			if (vendor === "Mastercard") {
-				card.forEach(side => {
-					side.classList.add("card--master");
-					const cardMaster = document.querySelectorAll(".card--master");
-					const logoMaster = document.querySelector(".card__logo--master");
+	subscribe(eventName, fn) {
+		if (!this.events[eventName]) {
+			this.events[eventName] = [];
+		}
 
-					cardMaster.forEach(each => {
-						each.style.display = "inline-block";
-					});
+		this.events[eventName].push(fn);
 
-					logoMaster.style.display = "block";
-				});
-			}
+		return () => {
+			this.events[eventName] = this.events[eventName].filter(
+				eventFn => fn !== eventFn
+			);
+		};
+	}
+
+	emit(eventName, data) {
+		const event = this.events[eventName];
+
+		if (event) {
+			event.forEach(fn => {
+				fn.call(null, data);
+			});
+		}
+	}
+}
+
+const emitter = new EventEmitter();
+emitter.subscribe("cardnumber", data => {
+	if (data.vendor === "Mastercard") {
+		card.forEach(side => {
+			side.classList.toggle("card--master");
+			const cardMaster = document.querySelectorAll(".card--master");
+			const logoMaster = document.querySelector(".card__logo--master");
+
+			cardMaster.forEach(each => {
+				each.style.display = "inline-block";
+			});
+
+			logoMaster.style.display = "block";
 		});
 	}
-})();
+});
+
+input.addEventListener("input", () => {
+	const vendor = obj.getCreditCardNameByNumber(input.value);
+	emitter.emit("cardnumber", { vendor });
+});
